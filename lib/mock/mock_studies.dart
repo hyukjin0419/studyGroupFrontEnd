0e5a04a21ac5598a980dde0530b8b72888ba07d4
hyukjin0419 <21900771@handong.ac.kr>
2025-04-22 22:51:10 +0900

    mock is added

---
+++ lib/mock/mock_studies.dart
@@ -0,0 +1,6 @@
+import '../models/study.dart';
+import '../mock/mock_users.dart';
+
+final mockStudies =[
+  Study(id:1, title:"Spring 프로젝트", members: mockUsers)
+];
---
+++ lib/mock/mock_users.dart
@@ -0,0 +1,7 @@
+import '../models/user.dart';
+
+final mockUsers = [
+  User(id:1, name: '유진', role:'Leader'),
+  User(id:2, name: '민수', role:'Fellow'),
+  User(id:3, name: '세영', role:'Fellow'),
+];
