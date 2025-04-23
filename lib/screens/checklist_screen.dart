9110a0c5d20ef1d01967df83060bccbf4d6294dc
hyukjin0419 <21900771@handong.ac.kr>
2025-04-23 11:32:15 +0900

    checklist_screen add button 추가

--- lib/screens/checklist_screen.dart
+++ lib/screens/checklist_screen.dart
@@ -33,10 +33,43 @@ class _ChecklistScreenState extends State<ChecklistScreen>{
     });
   }
 
+  void _addItem(String content){
+    setState(() {
+      items.add(ChecklistItem(
+          id: items.length+1,
+          content: content,
+          isCompleted: false,
+          createdByUserId: 1
+      ));
+    });
+  }
+
+  void _showAddDialog(BuildContext context){
+    final controller = TextEditingController();
+    showDialog(
+        context: context,
+        builder: (_)=> AlertDialog(
+          title: Text("새 항목 추가"),
+          content: TextField(controller: controller),
+          actions: [
+            TextButton(onPressed: () => Navigator.pop(context), child: Text('취소')),
+            TextButton(
+              onPressed: () {
+                final content = controller.text.trim();
+                if(content.isNotEmpty) _addItem(content);
+                Navigator.pop(context);
+              },
+              child: Text('추가'),
+            ),
+          ],
+        ),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('팀 체크리스트')),
+      appBar: AppBar(title: Text('${widget.study.title}의 체크리스트')),
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context,index){
@@ -46,6 +79,10 @@ class _ChecklistScreenState extends State<ChecklistScreen>{
               onChange: () => toggleItem(index),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _showAddDialog(context),
+          child: Icon(Icons.add),
       ),
     );
   }
