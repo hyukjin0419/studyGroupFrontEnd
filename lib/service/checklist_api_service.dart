8594ff65e7b937ced22ee0518a7afe86f765af40
hyukjin0419 <21900771@handong.ac.kr>
2025-05-18 11:49:02 +0900

    checlist_api_service

---
+++ lib/service/checklist_api_service.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:study_group_front_end/models/checklist_member.dart';
+
+class ChecklistApiService {
+  final String baseUrl;
+  final http.Client httpClient;
+  final String basePath = '/checklist-mebmers';
+
+  ChecklistApiService({required this.baseUrl, http.Client? client})
+    : httpClient = client ?? http.Client();
+
+  Uri _uri(String endpoint) => Uri.parse('$baseUrl$basePath$endpoint');
+
+  Future<ChecklistMemberAssignResDto> assignChecklist(ChecklistMemberAssignReqDto request) async {
+    final response = await httpClient.post(
+        _uri('/assign'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return ChecklistMemberAssignResDto.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("assignChecklist 실패: ${response.statusCode}");
+    }
+  }
+
+  Future<ChecklistMemberChangeStatusResDto> changeChecklistStatus(ChecklistMemberAssignReqDto request) async {
+    final response = await httpClient.patch(
+      _uri('/change-status'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return ChecklistMemberChangeStatusResDto.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("change checklist status 실패: ${response.statusCode}");
+    }
+  }
+
+  Future<ChecklistMemberUnassignResDto> unassignChecklist(int checklistId, int memberId) async{
+    final response = await httpClient.delete(
+      _uri('/$checklistId/members/$memberId'),
+    );
+
+    if (response.statusCode == 200) {
+      return ChecklistMemberUnassignResDto.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("unassign checklist 실패: ${response.statusCode}");
+    }
+  }
+
+  Future<List<ChecklistMemberResDto>> getChecklistsByMemberId(int memberId) async {
+    final response = await httpClient.get(_uri('/member/$memberId'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = jsonDecode(utf8.decode(response.bodyBytes));
+      return jsonList.map((e) => ChecklistMemberResDto.fromJson(e)).toList();
+    } else {
+      throw Exception("checklist by member Id 조회 실패: ${response.statusCode}");
+    }
+  }
+
+  Future<List<StudyChecklistMemberResDto>> getChecklistByStudyId(int studyId) async{
+    final response = await httpClient.get(_uri('/study/$studyId'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = jsonDecode(utf8.decode(response.bodyBytes));
+      return jsonList.map((e) => StudyChecklistMemberResDto.fromJson(e)).toList();
+    } else {
+      throw Exception("checklist by study Id 조회 실패: ${response.statusCode}");
+    }
+  }
+}
