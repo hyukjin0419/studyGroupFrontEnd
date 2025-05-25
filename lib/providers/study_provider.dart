95706b1112d0311941bce827ffc1631ce7d62a63
hyukjin0419 <21900771@handong.ac.kr>
2025-05-25 14:30:02 +0900

    study_provider.dart

--- lib/providers/study_provider.dart
+++ lib/providers/study_provider.dart
@@ -1,19 +1,83 @@
 import 'package:flutter/material.dart';
-import '../models/study.dart';
-import '../mock/mock_studies.dart';
+import 'package:study_group_front_end/models/study.dart';
+import 'package:study_group_front_end/service/study_api_service.dart';
 
-class StudyProvider extends ChangeNotifier {
-  final List<Study> _studies = [...mockStudies];
+class StudyProvicder with ChangeNotifier {
+  final StudyApiService apiService;
 
-  List<Study> get studies => _studies;
+  StudyProvicder({required this.apiService});
 
-  void addStudy(Study study){
-    _studies.add(study);
-    notifyListeners();
+  List<StudyListResDto> _studies = [];
+  StudyDetailResDto? _selectedStudy;
+  bool _isLoading = false;
+
+  List<StudyListResDto> get studies => _studies;
+  StudyDetailResDto? get selectedStudy => _selectedStudy;
+  bool get isLoading => _isLoading;
+
+  Future<void> fetchStudies() async {
+    _setLoading(true);
+    try{
+      _studies = await apiService.getAllStudies();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setLoading(false);
+    }
   }
 
-  void removeStudy(int id){
-    _studies.removeWhere((s) => s.id == id);
+  Future<void> fetchStudyDetail(int id) async {
+    _setLoading(true);
+    try {
+      _selectedStudy = await apiService.getStudyById(id);
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  Future<void> createStudy(StudyCreateReqDto request) async {
+    _setLoading(true);
+    try {
+      final response = await apiService.createStudy(request);
+      await fetchStudies();
+      await fetchStudyDetail(response.id);
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  Future<void> updateStudy(int studyId, int leaderId, StudyUpdateReqDto request) async {
+    _setLoading(true);
+    try {
+      await apiService.updateStudy(studyId, leaderId, request);
+      await fetchStudies();
+      await fetchStudyDetail(studyId);
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  Future<void> deleteStudy(int studyId, int leaderId) async {
+    _setLoading(true);
+    try {
+      await apiService.deleteStudy(studyId, leaderId);
+      await fetchStudies();
+      _selectedStudy = null;
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  void _setLoading(bool value){
+    _isLoading = value;
     notifyListeners();
   }
 }
