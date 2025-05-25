e3cac0e722e10b2c9afae20315108291403e7a42
hyukjin0419 <21900771@handong.ac.kr>
2025-05-25 15:01:53 +0900

    member_provider에 loading notifier 추가

--- lib/providers/member_provider.dart
+++ lib/providers/member_provider.dart
@@ -1,51 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:study_group_front_end/models/member.dart';
+import 'package:study_group_front_end/providers/loading_notifier.dart';
 import 'package:study_group_front_end/service/member_api_service.dart';
 
-class MemberProvider with ChangeNotifier {
+class MemberProvider with ChangeNotifier,LoadingNotifier {
   final MemberApiService apiService;
 
   MemberProvider({required this.apiService});
 
   MemberDetailResDto? _currentMember;
-  bool _isLoading = false;
 
   MemberDetailResDto? get currentMember => _currentMember;
-  bool get isLoading => _isLoading;
+
+  //오리지널
+  // Future<void> login(MemberLoginReqDto request) async {
+  //   _setLoading(true);
+  //   try{
+  //     final loginRes = await apiService.login(request);
+  //     final member = await apiService.getMemberById(loginRes.id);
+  //     _currentMember = member;
+  //   } catch (e) {
+  //     rethrow;
+  //   } finally {
+  //     _setLoading(false);
+  //   }
+  // }
 
   Future<void> login(MemberLoginReqDto request) async {
-    _setLoading(true);
-    try{
+    await runWithLoading(() async {
       final loginRes = await apiService.login(request);
       final member = await apiService.getMemberById(loginRes.id);
       _currentMember = member;
-    } catch (e) {
-      rethrow;
-    } finally {
-      _setLoading(false);
-    }
+    });
   }
 
   Future<void> create(MemberCreateReqDto request) async {
-    _setLoading(true);
-    try {
+    await runWithLoading(() async {
       final created = await apiService.createMember(request);
       final member = await apiService.getMemberById(created.id);
       _currentMember = member;
-    } catch (e) {
-      rethrow;
-    } finally {
-      _setLoading(false);
-    }
+    });
+  }
+
+  Future<void> update(MemberUpdateReqDto request) async {
+    await runWithLoading(() async {
+      final updated = await apiService.updateMember(request);
+      _currentMember = updated;
+    });
+  }
+
+  Future<void> delete(int id) async {
+    await runWithLoading(() async {
+      await apiService.deleteMember(id);
+      _currentMember = null;
+    });
   }
 
   void logout() {
     _currentMember = null;
-    notifyListeners();
-  }
-
-  void _setLoading(bool value){
-    _isLoading = value;
     notifyListeners();
   }
 }
