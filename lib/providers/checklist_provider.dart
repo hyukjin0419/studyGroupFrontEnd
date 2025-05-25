3e1f1c788452eff7caa7e67cf30fc96b70738786
hyukjin0419 <21900771@handong.ac.kr>
2025-05-25 15:19:31 +0900

    checklist_provider

--- lib/providers/checklist_provider.dart
+++ lib/providers/checklist_provider.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
-import '../models/checklist.dart';
-import '../mock/mock_checklists.dart';
+import 'package:study_group_front_end/models/checklist.dart';
+import 'package:study_group_front_end/providers/loading_notifier.dart';
+import 'package:study_group_front_end/service/checklist_api_service.dart';
 
-class ChecklistProvider extends ChangeNotifier{
-  final List<ChecklistItem> _items = [...mockChecklistItems];
+class ChecklistProvider with ChangeNotifier, LoadingNotifier {
+  final ChecklistApiService apiService;
 
-  List<ChecklistItem> get items => _items;
+  ChecklistProvider({required this.apiService});
 
-  void toggleItem(int index){
-    final item = _items[index];
-    _items[index] = ChecklistItem(
-        id: item.id,
-        content: item.content,
-        isCompleted: !item.isCompleted,
-        createdByUserId: item.createdByUserId
-    );
-    notifyListeners();
+  ChecklistDetailResDto? _selectedChecklist;
+  ChecklistDetailResDto? get selectedChecklist => _selectedChecklist;
+
+  Future<void> createChecklist(int creatorId, ChecklistCreateReqDto req) async {
+    await runWithLoading(() async {
+      final res = await apiService.createChecklist(creatorId, req);
+      _selectedChecklist = await apiService.getChecklistDetail(res.id);
+    });
   }
 
-  void addItem(String content){
-    final newItem = ChecklistItem(id: _items.length+1,
-        content: content,
-        isCompleted: false,
-        createdByUserId: 1);
-    _items.add(newItem);
+  Future<void> fetchChecklistDetail(int checklistId) async {
+    await runWithLoading(() async {
+      _selectedChecklist = await apiService.getChecklistDetail(checklistId);
+    });
+  }
+
+  Future<void> updateContent(int checklistId, String content) async {
+    await runWithLoading(() async {
+      await apiService.updateContent(checklistId, content);
+      _selectedChecklist = await apiService.getChecklistDetail(checklistId);
+    });
+  }
+
+  Future<void> updateDueDate(int checklistId, DateTime dueDate) async {
+    await runWithLoading(() async {
+      await apiService.updateDueDate(checklistId, dueDate);
+      _selectedChecklist = await apiService.getChecklistDetail(checklistId);
+    });
+  }
+
+  Future<void> deleteChecklist(int checklistId) async {
+    await runWithLoading(() async {
+      await apiService.deleteChecklist(checklistId);
+      _selectedChecklist = null;
+    });
+  }
+
+  void clearSelectedChecklist() {
+    _selectedChecklist = null;
     notifyListeners();
   }
 }
