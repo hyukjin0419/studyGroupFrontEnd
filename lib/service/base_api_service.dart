508b9e911c0ea23c1716c7db0daae61389f05ff5
hyukjin0419 <21900771@handong.ac.kr>
2025-06-29 14:20:02 +0900

    base_api

--- lib/service/base_api_service.dart
+++ lib/service/base_api_service.dart
@@ -2,17 +2,26 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 abstract class BaseApiService {
-  final String baseUrl;
-  final http.Client httpClient;
+  final String baseUrl = 'http://localhost:8080';
 
-  BaseApiService({
-    required this.baseUrl,
-    http.Client? client,
-  }) : httpClient = client ?? http.Client();
 
-  Uri uri(String basePath, String endpoint) => Uri.parse('$baseUrl$basePath$endpoint');
-
-  dynamic decodeJson(http.Response response) {
-    return jsonDecode(utf8.decode(response.bodyBytes));
+  Future<http.Response> get(String path, {bool authRequired = true}) async {
+    return await _requestWithRetry(() async {
+      final headers = await _buildHeaders(authRequired);
+      return await http.get(Uri.parse('$_baseUrl$path'), headers: headers);
+    });
   }
+
+  Future<http.Response> post()
+
+// BaseApiService({
+//   required this.baseUrl,
+//   http.Client? client,
+// }) : httpClient = client ?? http.Client();
+//
+// Uri uri(String basePath, String endpoint) => Uri.parse('$baseUrl$basePath$endpoint');
+//
+// dynamic decodeJson(http.Response response) {
+//   return jsonDecode(utf8.decode(response.bodyBytes));
+// } {
 }
