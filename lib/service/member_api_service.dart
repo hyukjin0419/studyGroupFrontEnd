eb8d20a917b8ca40fe53fa0286771fd3db0a71db
hyukjin0419 <21900771@handong.ac.kr>
2025-06-30 16:15:33 +0900

    member_api_service 리팩토링 + 주석처리

--- lib/service/member_api_service.dart
+++ lib/service/member_api_service.dart
@@ -1,89 +1,54 @@
-import 'dart:convert';
-import 'package:study_group_front_end/models/member.dart';
-import 'package:study_group_front_end/service/base_api_service.dart';
-
-class  MemberApiService extends BaseApiService{
-  final String basePath = '/members';
-
-  MemberApiService({
-    required super.baseUrl,
-    super.client,
-  });
-
-  Future<MemberCreateResDto> createMember(MemberCreateReqDto request) async {
-    final response = await httpClient.post(
-      uri(basePath,''),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(request.toJson()),
-    );
-
-    if (response.statusCode == 200) {
-      return MemberCreateResDto.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('create Member failed: ${response.statusCode}');
-    }
-  }
-
-  Future<MemberLoginResDto> login(MemberLoginReqDto request) async {
-    final response = await httpClient.post(
-      uri(basePath, '/login'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(request.toJson()),
-    );
-
-    if (response.statusCode == 200) {
-      return MemberLoginResDto.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('login failed: ${response.statusCode}');
-    }
-  }
-
-  Future<MemberDetailResDto> getMemberById(int id) async {
-    final response = await httpClient.get(
-      uri(basePath, '/$id'),
-    );
-
-    if (response.statusCode == 200) {
-      return MemberDetailResDto.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('get member by id failed: ${response.statusCode}');
-    }
-  }
-
-  Future<List<MemberDetailResDto>> getAllMembers() async {
-    final response = await httpClient.get(
-      uri(basePath, ''),
-    );
-
-    if (response.statusCode == 200) {
-      final List<dynamic> jsonList = jsonDecode(utf8.decode(response.bodyBytes));
-      return jsonList.map((e) => MemberDetailResDto.fromJson(e)).toList();
-    } else {
-      throw Exception('get all members failed: ${response.statusCode}');
-    }
-  }
-
-  Future<MemberDetailResDto> updateMember(MemberUpdateReqDto request) async {
-    final response = await httpClient.post(
-      uri(basePath, '/update'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(request.toJson()),
-    );
-
-    if (response.statusCode == 200) {
-      return MemberDetailResDto.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('update member failed: ${response.statusCode}');
-    }
-  }
-
-  Future<MemberDeleteResDto> deleteMember(int id) async{
-    final response = await httpClient.delete(uri(basePath,'$id'));
-
-    if (response.statusCode == 200) {
-      return MemberDeleteResDto.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('delete member failed: ${response.statusCode}');
-    }
-  }
-}
+// import 'dart:convert';
+// import 'package:study_group_front_end/dto/member/delete/member_delete_response.dart';
+// import 'package:study_group_front_end/dto/member/detail/member_detail_response.dart';
+// import 'package:study_group_front_end/dto/member/update/member_update_request.dart';
+// import 'package:study_group_front_end/service/base_api_service.dart';
+//
+// //관리자용 -> 현재 사용 x
+// class  MemberApiService extends BaseApiService{
+//   final String basePath = '/members';
+//
+//   Future<MemberDetailResponse> getMemberById(int id) async {
+//     final response = await get('$basePath/$id)');
+//
+//     if (response.statusCode == 200) {
+//       return MemberDetailResponse.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception('get member by id failed: ${response.statusCode}');
+//     }
+//   }
+//
+//   Future<List<MemberDetailResponse>> getAllMembers() async {
+//     final response = await get(basePath);
+//
+//     if (response.statusCode == 200) {
+//       final List<dynamic> jsonList = jsonDecode(utf8.decode(response.bodyBytes));
+//       return jsonList.map((e) => MemberDetailResponse.fromJson(e)).toList();
+//     } else {
+//       throw Exception('get all members failed: ${response.statusCode}');
+//     }
+//   }
+//
+//   Future<MemberDetailResponse> updateMember(int id, MemberUpdateRequest request) async {
+//     final response = await post(
+//       '$basePath/$id',
+//       request.toJson(),
+//     );
+//
+//     if (response.statusCode == 200) {
+//       return MemberDetailResponse.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception('update member failed: ${response.statusCode}');
+//     }
+//   }
+//
+//   Future<MemberDeleteResponse> deleteMember(int id) async{
+//     final response = await delete('$basePath/$id');
+//
+//     if (response.statusCode == 200) {
+//       return MemberDeleteResponse.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception('delete member failed: ${response.statusCode}');
+//     }
+//   }
+// }
