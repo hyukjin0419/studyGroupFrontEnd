dd0dc8a8904611343cf8f48c64fb8f97b4762efe
hyukjin0419 <21900771@handong.ac.kr>
2025-07-01 21:38:27 +0900

    GoRouter 초기 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:study_group_front_end/providers/me_provider.dart';
-import 'package:study_group_front_end/providers/study_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:study_group_front_end/providers/me_provider.dart';
 import 'package:study_group_front_end/router.dart';
-import 'package:study_group_front_end/screens/LoginScreen.dart';
 import 'package:study_group_front_end/service/auth_api_service.dart';
 import 'package:study_group_front_end/service/me_api_service.dart';
-import 'package:study_group_front_end/service/member_api_service.dart';
-import 'package:study_group_front_end/service/study_api_service.dart';
 
 void main() {
   runApp(
