96098818add818df1ea9979ce14a7ba0a5b80c88
hyukjin0419 <21900771@handong.ac.kr>
2025-07-14 19:24:19 +0900

    studycard에 vert 추가 및 스타일 수정

--- lib/screens/study/widgets/study_card.dart
+++ lib/screens/study/widgets/study_card.dart
@@ -1,4 +1,3 @@
-// study/widgets/study_card.dart
 import 'package:flutter/material.dart';
 import 'package:study_group_front_end/dto/study/detail/study_detail_response.dart';
 import 'package:study_group_front_end/util/color_converters.dart';
@@ -11,27 +10,52 @@ class StudyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      // margin: EdgeInsets.zero,
-      // elevation: 0,
       color: hexToColor(study.personalColor),
-      // shape: RoundedRectangleBorder(
-      //   borderRadius: BorderRadius.circular(12),
-      // ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(study.name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white), maxLines: 1),
-            const SizedBox(height: 8),
-            LinearProgressIndicator(value: study.progress, backgroundColor: Colors.white38, color: Colors.white),
-            const SizedBox(height: 4),
-            Text('${(study.progress * 100).round()}%', style: const TextStyle(color: Colors.white, fontSize: 12)),
-            const Spacer(),
-            Text(study.dueDate.toString(), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600)),
-            Text(study.status, style: const TextStyle(color: Colors.white)),
-          ],
-        ),
+      child: Stack(
+        children: [
+          Positioned(
+              top: 2,
+              right: -8,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                  size: 18,
+                ),
+                onPressed: () {
+
+                },
+              )
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(study.name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white), maxLines: 1),
+                const SizedBox(height: 8),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: SizedBox(
+                    height: 6,
+                    child:
+                    LinearProgressIndicator(
+                      value: study.progress,
+                      backgroundColor: Colors.white38,
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text('${(study.progress * 100).round()}%', style: const TextStyle(color: Colors.white, fontSize: 12)),
+                const Spacer(),
+                Text(study.dueDate.toString(), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600)),
+                Text(study.status, style: const TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
