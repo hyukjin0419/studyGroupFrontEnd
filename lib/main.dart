11dcdedc6a61722162fe031646e160cfc4ec6cf1
hyukjin0419 <21900771@handong.ac.kr>
2025-07-19 17:19:30 +0900

    firebase ios 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:study_group_front_end/firebase_options.dart';
 import 'package:study_group_front_end/providers/me_provider.dart';
 import 'package:study_group_front_end/providers/study_provider.dart';
 import 'package:study_group_front_end/router.dart';
@@ -7,8 +10,14 @@ import 'package:study_group_front_end/service/auth_api_service.dart';
 import 'package:study_group_front_end/service/me_api_service.dart';
 import 'package:study_group_front_end/service/study_api_service.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
+  await FirebaseMessaging.instance.requestPermission();
+
   runApp(
       MultiProvider(
         providers: [
