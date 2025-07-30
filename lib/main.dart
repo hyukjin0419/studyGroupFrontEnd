aa547b4a003e98c4c872260d1b2facac1f9a3c6c
hyukjin0419 <21900771@handong.ac.kr>
2025-07-31 00:15:11 +0900

    fcm service refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,12 @@ import 'package:study_group_front_end/api_service/me_api_service.dart';
 import 'package:study_group_front_end/api_service/study_api_service.dart';
 import 'package:study_group_front_end/api_service/study_join_api_service.dart';
 import 'package:study_group_front_end/firebase_options.dart';
-import 'package:study_group_front_end/notification_service/fcm_initializer.dart';
-import 'package:study_group_front_end/notification_service/outdated/firebase_messaginig_service.dart';
+import 'package:study_group_front_end/notification_service/fcm/fcm_initializer.dart';
 import 'package:study_group_front_end/notification_service/local/local_notifications_service.dart';
 import 'package:study_group_front_end/providers/me_provider.dart';
 import 'package:study_group_front_end/providers/study_join_provider.dart';
 import 'package:study_group_front_end/providers/study_provider.dart';
 import 'package:study_group_front_end/router.dart';
-import 'package:study_group_front_end/util/navigator_key.dart';
 
 Future<void> main() async {
   //비동기 작업 전에 Flutter 프레임워크 초기화 보장 -> 원래는 runApp에서 자동 초기화 되는데, 그 전에 초기화 해주어야해서
