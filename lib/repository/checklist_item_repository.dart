a7f82d8a6cfc817c34034eb83de1233e6a5fbb6c
hyukjin0419 <21900771@handong.ac.kr>
2025-10-19 01:02:48 +0900

    delete: deleted unusing codes in checklist_item_repository.dart

--- lib/repository/checklist_item_repository.dart
+++ lib/repository/checklist_item_repository.dart
@@ -84,10 +84,6 @@ class InMemoryChecklistItemRepository{
     return List.of(list);
   }
 
-  //개인용 init fetch
-
-
-
   // ===========================================================
   // 📡 STREAM  단순 채널 열고 캐시 데이터 방출 -> 캐시 데이터 로드 여기서 금지 -> 채널 열기만 하기
   // ===========================================================
@@ -171,37 +167,6 @@ class InMemoryChecklistItemRepository{
   void _saveToCacheAndStream(List<ChecklistItemDetailResponse> items){
     _apiToCache(items);
     _cacheToAllStreams();
-    // log("saveToCacheAndStream");
-    // for(final item in items){
-    //   final dateKey = _dateKey(item.targetDate);
-    //   _cache.putIfAbsent(dateKey,() => []);
-    //   final list = _cache[dateKey]!;
-    //
-    //   final idx = list.indexWhere((e) => e.id == item.id);
-    //   if(idx >= 0){
-    //     list[idx] = item;
-    //   } else {
-    //     list.add(item);
-    //   }
-    //
-    //   final teamStreamKey = 'team_${item.studyId}_$dateKey';
-    //   _streams.putIfAbsent(teamStreamKey, () => StreamController.broadcast());
-    //   if(_streams.containsKey(teamStreamKey)) {
-    //     final teamItems = list.where((e) => e.studyId == item.studyId).toList();
-    //
-    //     _streams[teamStreamKey]!.add(teamItems);
-    //   }
-    //
-    //   if(item.studyMemberId == currentMemberId){
-    //     final personalStreamKey = 'personal_$dateKey';
-    //     _streams.putIfAbsent(personalStreamKey, () => StreamController.broadcast());
-    //     log("teamStreamKey in saveToCacheAndStream $teamStreamKey}");
-    //     if(_streams.containsKey(personalStreamKey)) {
-    //       final personalItems = list.where((e) => e.studyMemberId == currentMemberId).toList();
-    //       _streams[personalStreamKey]!.add(personalItems);
-    //     }
-    //   }
-    // }
   }
 
 
@@ -214,78 +179,8 @@ class InMemoryChecklistItemRepository{
     log("🗓️ [Cache] ${startOfWeek.toString().split(' ').first} 주차의 7일 캐시 초기화 완료 (총 ${_cache.length}일)");
   }
 
-// //--------------------우선적으로 캐시 호출--------------------//
-//   //force=true시 캐시가 있어도 무시하고 서버 데이터로 덮음
-//   //UI에서 특정날짜 선택시 해당하는 날짜에 대한 checklistItem을 return 해주는 함수
-//   Future<List<ChecklistItemDetailResponse>> getChecklistItems(int studyId, DateTime date, {bool force = false}) async {
-//     log("cache에서 체크리스트 데이터를 찾는 중....");
-//     final key = _key(studyId, date);
-//     if(_cache.containsKey(key) && !force) return _cache[key]!;
-//     return await _fetchWeek(studyId, date);
-//   }
-
-//--------------------캐시에서 데이터 못찾을 시 fetch 주 단위로 Update--------------------//
-  //date가 속한 주의 시작일 (월요일) 기준으로 7일씩 묶어서 가져오기
-  // Future<List<ChecklistItemDetailResponse>> _fetchWeek(int studyId, DateTime date, {bool pushToStream = false}) async {
-  //   log("_fetchByWeek");
-  //   final startOfWeek = date.subtract(Duration(days: date.weekday - 1));
-  //   final list = await api.getChecklistItemsOfStudyByWeek(studyId, startOfWeek);
-  //
-  //   //1. 받은 데이터는 날짜별 그룹핑해서 캐시 저장
-  //   _saveToCacheAndStream(list);
-  //
-  //   // 2. 해당 주의 모든 날짜를 캐시에 등록 (데이터가 없어도 []로)
-  //   for (int i = 0; i < 7; i++) {
-  //     final d = startOfWeek.add(Duration(days: i));
-  //     final key = _key(studyId, d);
-  //     _cache.putIfAbsent(key, () => []);
-  //     if (pushToStream && _streams.containsKey(key)) {
-  //       _streams[key]!.add(_cache[key]!);
-  //     }
-  //   }
-  //
-  //   // 호출자가 요청한 날짜 데이터만 반환
-  //   final requestedKey = _key(studyId, date);
-  //   _cache.putIfAbsent(requestedKey, () => []);
-  //   return _cache[requestedKey] ?? [];
-  // }
-  //
-  // //일별 단일 호출
-  // Future<List<ChecklistItemDetailResponse>> _fetchDay(int studyId, DateTime date, {bool pushToStream = true}) async {
-  //   log("cache에서 체크리스트 데이터를 찾을 수 없습니다. fetch from server 동작");
-  //   final key = _key(studyId, date);
-  //   final list = await api.getChecklistItemsOfStudyByDay(studyId, date);
-  //   _cache[key] = list;
-  //   if (pushToStream && _streams.containsKey(key)) _streams[key]!.add(list);
-  //   return list;
-  // }
-  //
-  //
-  // //받아온 체크리스트 스터디 + 날짜별로 나눠서 캐시에 반영 + 스트림에 흘려보내기
-  // void _saveToCacheAndStream(
-  //     List<ChecklistItemDetailResponse> items, {
-  //       bool pushToStream = true,
-  //     }) {
-  //   // 날짜별로 그룹핑
-  //   final Map<String, List<ChecklistItemDetailResponse>> grouped = {};
-  //   for (final item in items) {
-  //     final key = _key(item.studyId, item.targetDate);
-  //     grouped.putIfAbsent(key, () => []);
-  //     grouped[key]!.add(item);
-  //   }
-  //
-  //   // 캐시에 반영 + 스트림에 push
-  //   grouped.forEach((key, value) {
-  //     _cache[key] = value;
-  //     if (pushToStream && _streams.containsKey(key)) {
-  //       _streams[key]!.add(value);
-  //     }
-  //   });
-  // }
-
 
 //--------------------Optimistic Update--------------------//
-
   // ===========================================================
   // ✏️ CRUD / REORDER
   // ===========================================================
